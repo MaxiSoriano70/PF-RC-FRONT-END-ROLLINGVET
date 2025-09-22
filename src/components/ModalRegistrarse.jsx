1bbee7f2c80f5ebae89086ea3a317e7770c6b02c
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-09-22 06:13:56 -0300

    Se agrego primera prueba de validaciones

--- src/components/ModalRegistrarse.jsx
+++ src/components/ModalRegistrarse.jsx
@@ -4,6 +4,50 @@ import styles from "../css/modalRegistrarse.module.css";
 function ModalRegistrarse({ show, handleClose }) {
   const [showPassword, setShowPassword] = useState(false);
   const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+
+  const regex = {
+    nombre: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,}$/,
+    apellido: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,}$/,
+    domicilio: /^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9\s.,]{3,}$/,
+    email: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/,
+    telefono: /^\+?[0-9]{8,15}$/,
+  };
+
+  const [formValues, setFormValues] = useState({
+    nombre: "",
+    apellido: "",
+    domicilio: "",
+    telefono: "",
+    email: "",
+    password: "",
+    passwordRepeat: "",
+  });
+
+  const [validity, setValidity] = useState({
+    nombre: null,
+    apellido: null,
+    domicilio: null,
+    telefono: null,
+    email: null,
+    password: null,
+    passwordRepeat: null,
+  });
+
+  const handleChange = (field, value) => {
+    setFormValues({ ...formValues, [field]: value });
+
+    if (field === "passwordRepeat") {
+      setValidity({
+        ...validity,
+        passwordRepeat: value === formValues.password,
+      });
+    } else {
+      setValidity({
+        ...validity,
+        [field]: regex[field].test(value),
+      });
+    }
   };
 
   return (
@@ -29,59 +73,98 @@ function ModalRegistrarse({ show, handleClose }) {
             <form id="form-registro">
               <div className="d-flex gap-3">
                 <div className="mb-3 w-100">
-                  <label htmlFor="nombreRegistro" className="form-label fw-bold">
+                  <label
+                    htmlFor="nombreRegistro"
+                    className="form-label fw-bold"
+                  >
                     Nombre
                   </label>
                   <input
                     type="text"
-                    className="form-control bgInput"
                     id="nombreRegistro"
                     placeholder="Ingrese su nombre"
-                    minLength="3"
-                    maxLength="25"
+                    className={`form-control bgInput ${validity.nombre === null
+                        ? ""
+                        : validity.nombre
+                          ? "is-valid"
+                          : "is-invalid"
+                      }`}
+                    value={formValues.nombre}
+                    onChange={(e) => handleChange("nombre", e.target.value)}
                     required
                   />
+
                 </div>
 
                 <div className="mb-3 w-100">
-                  <label htmlFor="apellidoRegistro" className="form-label fw-bold">
+                  <label
+                    htmlFor="apellidoRegistro"
+                    className="form-label fw-bold"
+                  >
                     Apellido
                   </label>
                   <input
                     type="text"
-                    className="form-control bgInput"
                     id="apellidoRegistro"
                     placeholder="Ingrese su apellido"
                     maxLength="25"
+                    className={`form-control bgInput ${validity.apellido === null
+                        ? ""
+                        : validity.apellido
+                          ? "is-valid"
+                          : "is-invalid"
+                      }`}
+                    value={formValues.apellido}
+                    onChange={(e) => handleChange("apellido", e.target.value)}
                     required
                   />
                 </div>
               </div>
 
               <div className="mb-3 w-100">
-                <label htmlFor="domicilioRegistro" className="form-label fw-bold">
+                <label
+                  htmlFor="domicilioRegistro"
+                  className="form-label fw-bold"
+                >
                   Domicilio
                 </label>
                 <input
                   type="text"
-                  className="form-control bgInput"
                   id="domicilioRegistro"
                   placeholder="Ingrese su domicilio"
                   maxLength="25"
+                  className={`form-control bgInput ${validity.domicilio === null
+                        ? ""
+                        : validity.domicilio
+                          ? "is-valid"
+                          : "is-invalid"
+                      }`}
+                    value={formValues.domicilio}
+                    onChange={(e) => handleChange("domicilio", e.target.value)}
                   required
                 />
               </div>
 
               <div className="mb-3">
-                <label htmlFor="telefonoRegistro" className="form-label fw-bold">
+                <label
+                  htmlFor="telefonoRegistro"
+                  className="form-label fw-bold"
+                >
                   Teléfono
                 </label>
                 <input
                   type="tel"
-                  className="form-control bgInput"
                   id="telefonoRegistro"
                   placeholder="Ingrese su teléfono"
                   maxLength="10"
+                  className={`form-control bgInput ${validity.telefono === null
+                        ? ""
+                        : validity.telefono
+                          ? "is-valid"
+                          : "is-invalid"
+                      }`}
+                    value={formValues.telefono}
+                    onChange={(e) => handleChange("telefono", e.target.value)}
                   required
                 />
               </div>
@@ -92,51 +175,80 @@ function ModalRegistrarse({ show, handleClose }) {
                 </label>
                 <input
                   type="email"
-                  className="form-control bgInput"
                   id="emailRegistro"
                   placeholder="Ingrese su email"
+                  className={`form-control bgInput ${validity.email === null
+                        ? ""
+                        : validity.email
+                          ? "is-valid"
+                          : "is-invalid"
+                      }`}
+                    value={formValues.email}
+                    onChange={(e) => handleChange("email", e.target.value)}
                   required
                 />
               </div>
 
               <div className="d-flex gap-3">
                 <div className="mb-3 w-100">
-                  <label htmlFor="passwordRegistro" className="form-label fw-bold">
+                  <label
+                    htmlFor="passwordRegistro"
+                    className="form-label fw-bold"
+                  >
                     Contraseña
                   </label>
                   <div className="position-relative">
                     <input
                       type={showPassword ? "text" : "password"}
-                      className="form-control bgInput pe-5"
                       id="passwordRegistro"
                       placeholder="Ingrese su contraseña"
                       minLength="8"
                       maxLength="20"
+                      className={`form-control bgInput pe-5 ${validity.password === null
+                        ? ""
+                        : validity.password
+                          ? "is-valid"
+                          : "is-invalid"
+                      }`}
+                      value={formValues.password}
+                      onChange={(e) => handleChange("password", e.target.value)}
                       required
                     />
                     <i
-                      className={`fa-solid ${showPassword ? "fa-eye" : "fa-eye-slash"} ${styles.buttonEye}`}
+                      className={`fa-solid ${showPassword ? "fa-eye" : "fa-eye-slash"
+                        } ${styles.buttonEye}`}
                       onClick={() => setShowPassword(!showPassword)}
                     ></i>
                   </div>
                 </div>
 
                 <div className="mb-3 w-100">
-                  <label htmlFor="passwordRegistroRepetir" className="form-label fw-bold">
+                  <label
+                    htmlFor="passwordRegistroRepetir"
+                    className="form-label fw-bold"
+                  >
                     Repetir contraseña
                   </label>
                   <div className="position-relative">
                     <input
                       type={showRepeatPassword ? "text" : "password"}
-                      className="form-control bgInput pe-5"
                       id="passwordRegistroRepetir"
                       placeholder="Repita su contraseña"
                       minLength="8"
                       maxLength="20"
+                      className={`form-control bgInput pe-5 ${validity.passwordRepeat === null
+                        ? ""
+                        : validity.passwordRepeat
+                          ? "is-valid"
+                          : "is-invalid"
+                      }`}
+                      value={formValues.passwordRepeat}
+                      onChange={(e) => handleChange("passwordRepeat", e.target.value)}
                       required
                     />
                     <i
-                      className={`fa-solid ${showRepeatPassword ? "fa-eye" : "fa-eye-slash"} ${styles.buttonEye}`}
+                      className={`fa-solid ${showRepeatPassword ? "fa-eye" : "fa-eye-slash"
+                        } ${styles.buttonEye}`}
                       onClick={() => setShowRepeatPassword(!showRepeatPassword)}
                     ></i>
                   </div>
